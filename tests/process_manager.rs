53bfde98b9c5737ecb598ebaf6b99dc568331a52
kazanefu <kazanefu12@gmail.com>
2026-05-10 23:55:15 +0900

    fix test

--- tests/process_manager.rs
+++ tests/process_manager.rs
@@ -7,15 +7,24 @@ async fn test_start_and_stop_process() {
     // choose platform-appropriate command
     #[cfg(windows)]
     let (exe, args): (PathBuf, Vec<String>) = {
-        (PathBuf::from("ping"), vec!["-n".to_string(), "4".to_string(), "127.0.0.1".to_string()])
+        (
+            PathBuf::from("ping"),
+            vec!["-n".to_string(), "4".to_string(), "127.0.0.1".to_string()],
+        )
     };
     #[cfg(not(windows))]
-    let (exe, args): (PathBuf, Vec<String>) = {
-        (PathBuf::from("sleep"), vec!["2".to_string()])
-    };
+    let (exe, args): (PathBuf, Vec<String>) = { (PathBuf::from("sleep"), vec!["2".to_string()]) };
 
     let id = "test-process";
-    let info = mgr.start(id, exe.clone(), &args.iter().map(|s| s.to_string()).collect::<Vec<_>>()).await.expect("start");
+    let info = mgr
+        .start(
+            id,
+            ".".into(),
+            exe.clone().into(),
+            &args.iter().map(|s| s.to_string()).collect::<Vec<_>>(),
+        )
+        .await
+        .expect("start");
     assert!(info.pid != 0);
     assert!(mgr.is_running(id).await);
     // stop
